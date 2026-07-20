c44a1875c999a5a1a41efee4c8c37db77a61bc90
digitalwellnessdesign <digitalwellnessdesigner@gmail.com>
2026-07-19 22:32:39 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/meg-feil/',
+  base: '/',
   plugins: [react()],
   build: {
     target: 'es2020',
